f9354ff90b6fcde15117136b6564ad8f593d051b
kudou98 <fanh210mon@gmail.com>
2022-04-10 07:45:19 +0000

    [Add]画像関連の修正

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,9 +5,9 @@ class Book < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true, length: { maximum: 200}
   
-  attachment :profile_image
   
-  has_one_attached :profile_image
+  
+  has_one_attached :profile_image #これに変えたい
 
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,12 +9,19 @@ class User < ApplicationRecord
   validates :introduction, length: { maximum: 50 }
 
   has_many :books, dependent: :destroy
+  
+  
 
-  attachment :profile_image
 
-
-
-  has_one_attached :profile_image
+  has_one_attached :profile_image #これに変えたい
+  
+  def get_profile_image(width, height)
+  unless profile_image.attached?
+    file_path = Rails.root.join('app/assets/images/no_image.jpg')
+    profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+  end
+  profile_image.variant(resize_to_limit: [width, height]).processed
+  end
 
 
 
